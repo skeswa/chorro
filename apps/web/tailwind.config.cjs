e44bc2edeca9bcd05bd1a7dad84c37b5718b5963
Sandile Keswa <me@sandile.io>
2022-01-17 12:46:32 -0600

    Formatted tings.

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
+  purge: ['./src/**/*.html', './src/**/*.svelte'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -11,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
