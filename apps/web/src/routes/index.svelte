fa00d4ef8865c1726a4e84ccbddfd11fc52642f9
Sandile Keswa <me@sandile.io>
2022-01-17 12:46:21 -0600

    Migrated chorro web to use tailwind

--- apps/web/src/routes/index.svelte
+++ apps/web/src/routes/index.svelte
@@ -4,43 +4,9 @@
 
 <svelte:head>
   <title>Chorro | Coming Soon</title>
-
-  <style>
-    html,
-    body {
-      margin: 0;
-      padding: 0;
-    }
-  </style>
 </svelte:head>
 
-<section>
-  <img src="/logo.svg" alt="Chorro logo" />
-  <p>coming soon</p>
+<section class="flex flex-col h-screen items-center justify-center w-screen">
+  <img alt="Chorro logo" class="w-64" src="/logo.svg" />
+  <p class="font-sans mt-6 text-4xl text-slate-800">coming soon</p>
 </section>
-
-<style>
-  section {
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-height: 100vh;
-    max-width: 100vw;
-    min-height: 100vh;
-    min-width: 100vw;
-  }
-
-  img {
-    max-height: 240px;
-    max-width: 240px;
-    min-height: 240px;
-    min-width: 240px;
-  }
-
-  p {
-    color: #3a425c;
-    font-family: sans-serif;
-    font-size: 36px;
-  }
-</style>
